b3dcf4e24cf146788068360f115c81ac107ebc93
yuliiasihetii <94548003+yuliiasihetii@users.noreply.github.com>
2022-04-01 20:54:25 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,41 @@
-let massage = prompt('What`s your name?');
+const operator = getOperator();
+const firstNumber = getNumber('Please, write a first number.');
+const secondNumber = getNumber('Please, write a second number.');
+let result = calculate(operator, firstNumber, secondNumber);
 
-alert('Hello,'+ massage + '! How are you?')
+showResult(operator, firstNumber, secondNumber, result);
+
+function getOperator() {
+    while (true) {
+        const operator = prompt('Please, write a mathematical operation.');
+        if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
+            return operator;
+        }
+    }
+}
+
+function getNumber(message) {
+    while (true) {
+        let result = prompt(message);
+        if (!isNaN(+result) && result != '') {
+            return result;
+        }
+    }
+}
+
+function calculate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case '+':
+            return firstNumber + secondNumber;
+        case '-':
+            return firstNumber - secondNumber;
+        case '*':
+            return firstNumber * secondNumber;
+        case '/':
+            return firstNumber / secondNumber;
+    }
+}
+
+function showResult(operator, firstNumber, secondNumber, result) {
+    alert(`${firstNumber} ${operator} ${secondNumber} = ${result}`);
+}
