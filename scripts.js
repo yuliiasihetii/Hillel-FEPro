96fc6a4444546d1b8620d1b9a887eedfd5c0af3f
yuliiasihetii <jsigetiy@gmail.com>
2022-04-09 01:46:02 +0300

    hm5

--- scripts.js
+++ scripts.js
@@ -1,3 +1,64 @@
-let massage = prompt('What`s your name?');
+const students = [
+    {
+        id: 10,
+        name: 'John Smith',
+        marks: [10, 8, 6, 9, 8, 7]
+    },
+    {
+        id: 11,
+        name: 'John Doe',
+        marks: [9, 8, 7, 6, 7]
+    },
+    {
+        id: 12,
+        name: 'Thomas Anderson',
+        marks: [6, 7, 10, 8]
+    },
+    {
+        id: 13,
+        name: 'Jean-Baptiste Emanuel Zorg',
+        marks: [10, 9, 8, 9]
+    }
+]
 
-alert('Hello,'+ massage + '! How are you?')
+const student = {
+    id: 10,
+    name: 'John Smith',
+    marks: [10, 8, 6, 9, 8, 7]
+}
+
+function avgStudent(students) {
+    return students.map((student) => {
+        const avrg = student.marks.reduce((acc, value) => {
+            return acc += value;
+        });
+
+        return {
+            name: student.name,
+            avrg: +(avrg / student.marks.length).toFixed(2)
+        };
+    })
+}
+
+
+function avrgStudents(students) {
+    const sudentsData = {
+        sumStudentsMarks: 0,
+        count: 0
+    }
+
+    students.forEach(student => {
+        student.marks.forEach(mark => {
+            sudentsData.sumStudentsMarks += mark;
+            sudentsData.count++;
+        });
+    });
+
+    return +(sudentsData.sumStudentsMarks / sudentsData.count).toFixed(2);
+}
+
+const avrgStudentReth = avgStudent(students);
+console.log(avrgStudentReth);
+
+const avrgStudentsReth = avrgStudents(students);
+console.log(avrgStudentsReth)
