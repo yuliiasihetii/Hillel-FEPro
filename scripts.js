06c60d3676453991de48abd748be2041353ef4c0
yuliiasihetii <jsigetiy@gmail.com>
2022-04-12 14:30:18 +0300

    hm6

--- scripts.js
+++ scripts.js
@@ -1,3 +1,39 @@
-let massage = prompt('What`s your name?');
+const obj = {
+    name: 'Alex',
+    age: 33,
+    adress: {
+        country: 'UA',
+        city: 'Dnipro',
+        marks: [],
+    },
+};
 
-alert('Hello,'+ massage + '! How are you?')
+const objCopy = copy(obj);
+
+function copy(obj) {
+    if (obj === null) return null;
+
+    let clone = Object.assign({}, obj);
+
+    Object.keys(clone).forEach(key => {
+        if (typeof obj[key] === "object") return clone[key] = copy(clone[key]);
+        else return clone[key];
+    });
+
+    if (Array.isArray(obj) && obj.length > 0) {
+        return (clone.length = obj.length) && Array.from(clone);
+    } else {
+        if (Array.isArray(obj)) {
+            return Array.from(obj)
+        } else {
+            return clone;
+        }
+    }
+}
+
+
+console.log(obj);
+console.log(objCopy);
+
+objCopy.adress.city = 'Kyiv';
+objCopy.adress.marks[0] = 4;
