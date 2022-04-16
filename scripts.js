15bc90f1e0358100f60a554c3cefc643326ac80c
yuliiasihetii <jsigetiy@gmail.com>
2022-04-16 03:30:41 +0300

    hm7

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
-let massage = prompt('What`s your name?');
+function createCalculator(n) {
+    return {
+        sum: (b) => n += b,
+        mult: (b) => n *= b,
+        sub: (b) => n -= b,
+        div: (b) => n /= b,
+        set: (b) => n = b,
+        get: () => n,
+    }
 
-alert('Hello,'+ massage + '! How are you?')
+}
+
+const calc = createCalculator(10);
+
+console.log(calc.sum(5))
+console.log(calc.mult(10))
+console.log(calc.sub(40))
+console.log(calc.div(10))
+console.log(calc.get())
