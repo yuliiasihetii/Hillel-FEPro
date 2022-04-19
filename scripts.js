0494e550127cc6428eeec307802e992286cddc62
yuliiasihetii <jsigetiy@gmail.com>
2022-04-19 17:34:36 +0300

    hm8

--- scripts.js
+++ scripts.js
@@ -1,3 +1,54 @@
-let massage = prompt('What`s your name?');
+const firstNumber = document.getElementById('first-number');
+const secondNumber = document.getElementById('second-number');
+const error = document.getElementById('error');
+const result = document.getElementById('result')
+const btn = document.getElementById('calculate-btn');
 
-alert('Hello,'+ massage + '! How are you?')
+btn.addEventListener('click', onBtnClick);
+
+function getFirstNumber(firstNumber) {
+    return firstNumber.value;
+}
+
+function getSecondNumber(secondNumber) {
+    return secondNumber.value;
+}
+
+function outputError() {
+    error.textContent = 'Operands are invalid';
+}
+
+function isFirstNumberValid(firstNumber) {
+    let result = getFirstNumber(firstNumber)
+    if (!isNaN(+result) && result != '') {
+        return +result;
+    }
+
+    return outputError();
+}
+
+function isSecondNumbeValid(secondNumber) {
+    let result = getFirstNumber(secondNumber)
+    if (!isNaN(+result) && result != '') {
+        return +result;
+    }
+
+    return outputError();
+}
+
+function addNumbers(firstNumber, secondNumber) {
+    const first = isFirstNumberValid(firstNumber);
+    const second = isSecondNumbeValid(secondNumber);
+
+    if (first != undefined && second != undefined) {
+        const res = first + second;
+
+        result.textContent = res;
+        error.textContent = '';
+    }
+}
+
+function onBtnClick() {
+    result.textContent = '';
+    addNumbers(firstNumber, secondNumber)
+}
