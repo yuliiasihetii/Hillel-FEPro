ee412e993ec97999052b4bf5457d8d7bccbf0db7
yuliiasihetii <jsigetiy@gmail.com>
2022-04-26 01:36:21 +0300

    hm9

--- scripts.js
+++ scripts.js
@@ -1,3 +1,45 @@
-let massage = prompt('What`s your name?');
+const listEl = document.getElementById('listEl');
+const taskEl = document.getElementById('taskEl');
+const errorEl = document.getElementById('error');
+const btn = document.getElementById('btn');
 
-alert('Hello,'+ massage + '! How are you?')
+btn.addEventListener('click', onAddBtnClick);
+
+function onAddBtnClick() {
+    clearError()
+    const el = getTask();
+    const isError = validateTask(el);
+
+    if (!isError) {
+        createList();
+    }
+
+    clearTask();
+}
+
+function getTask() {
+    return taskEl.value;
+}
+
+function clearTask() {
+    taskEl.value = '';
+}
+
+function createList() {
+    const li = document.createElement('li');
+    li.textContent = getTask();
+    listEl.append(li);
+}
+
+function validateTask(value) {
+    if (value = '' || value.length < 4) {
+        errorEl.textContent = 'Invalid Operator';
+        taskEl.classList.add('invalid');
+        return true;
+    }
+}
+
+function clearError() {
+    errorEl.textContent = '';
+    taskEl.classList.remove('invalid');
+}
