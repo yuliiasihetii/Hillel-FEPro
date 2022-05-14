19f0c99900dcfadca86d0c17766ba36a1a497a85
yuliiasihetii <jsigetiy@gmail.com>
2022-05-14 09:16:09 +0300

    hm14

--- scripts.js
+++ scripts.js
@@ -1,3 +1,69 @@
-let massage = prompt('What`s your name?');
+'use strict';
 
-alert('Hello,'+ massage + '! How are you?')
+const SIZE_SMALL = {
+    prise: 50,
+    callories: 20
+}
+
+const SIZE_MEDIUM = {
+    prise: 75,
+    callories: 30
+}
+
+const SIZE_BIG = {
+    prise: 100,
+    callories: 40
+}
+
+const TOPPING_CHEESE = {
+    prise: 10,
+    callories: 20
+}
+
+const TOPPING_SALAD = {
+    prise: 20,
+    callories: 5
+}
+
+const TOPPING_POTATO = {
+    prise: 15,
+    callories: 10
+}
+
+const TOPPING_SEASONING = {
+    prise: 15,
+    callories: 0
+}
+
+const TOPPING_MAYO = {
+    prise: 20,
+    callories: 5
+}
+
+class Hamburger {
+    constructor(obj) {
+        this.prise = obj.prise;
+        this.callories = obj.callories;
+    }
+
+    addTopping(topping) {
+        this.prise += topping.prise
+        this.callories += topping.callories
+    }
+
+    getPrice() {
+        return this.prise
+    }
+
+    getCallories() {
+        return this.callories
+    }
+}
+
+const hamburger = new Hamburger(SIZE_SMALL);
+
+hamburger.addTopping(TOPPING_MAYO);
+hamburger.addTopping(TOPPING_POTATO);
+
+console.log('Price with sauce: ' + hamburger.getPrice());
+console.log('Callories with sauce: ' + hamburger.getCallories());
