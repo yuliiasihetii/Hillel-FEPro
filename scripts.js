d5c0a1127e462281f605b28d9ba0900e40bca8c5
yuliiasihetii <jsigetiy@gmail.com>
2022-05-14 09:40:31 +0300

    hm10

--- scripts.js
+++ scripts.js
@@ -1,68 +1,74 @@
-const INPUT_CLASS = 'input';
-const ERROR_CLASS = 'error';
-const INPUT_WRAPPER_CLASS = 'input-wrapper';
-const BTN_REMOVE_CLASS = 'btn-remove'
-
+const TASK_ITEMS_TEMPLATE = document.getElementById('taskItemsTemplate').innerHTML
 const form = document.getElementById('table-form');
-const table = document.getElementById('table-contacts');
+const contactList = document.getElementById('contact-list');
+const nameInput = document.getElementById('name');
+const surnameInput = document.getElementById('surname');
+const phoneInput = document.getElementById('phone');
+const inputList = form.querySelectorAll('input');
+const errorEl = document.getElementById('error');
+const btn = document.getElementById('btn');
 
-form.addEventListener('submit', formController);
-table.addEventListener('click', removeRow);
+form.addEventListener('submit', onAddBtnClick);
+contactList.addEventListener('click', removeRow);
 
-function formController() {
-    let text = null;
+function onAddBtnClick(e) {
+    e.preventDefault();
 
-    const inputList = form.querySelectorAll('.' + INPUT_WRAPPER_CLASS);
+    clearError()
 
-    for (let i = 0; i < inputList.length; i++) {
-        text = inputList[i].querySelector(INPUT_CLASS).value.trim();
+    const formName = getValue(nameInput);
+    const formSurname = getValue(surnameInput);
+    const formPhoneData = getValue(phoneInput);
 
-        if (!isEmpty(text, inputList[i])) {
-            inputList[i].classList.add(ERROR_CLASS);
-            return;
-        } else {
-            inputList[i].classList.remove(ERROR_CLASS);
-        }
+    const isInputListEmpty = isEmpty(inputList);
+
+    if (!isInputListEmpty) {
+        addContact(formName, formSurname, formPhoneData);
+        clearInputs(inputList);
     }
-
-    addContact(inputList);
-    clearForm();
 }
 
-function isEmpty(text, element) {
-
-    if (text.length === 0) {
-        element.classList.add(ERROR_CLASS);
-        return false;
-    }
-
-    return true;
+function addContact(name, surname, phone) {
+    const html = createContactHTML(name, surname, phone);
+    contactList.insertAdjacentHTML("beforeend", html)
 }
 
-function addContact(inputList) {
-    let text = null;
-
-    let addElement = '<td class="none">';
-
-    inputList.forEach(element => {
-        text = element.querySelector(INPUT_CLASS).value.trim();
-
-        addElement += `<td class ="td"> ${text} </td>`;
-    });
-
-    addElement += '<td><span class="btn-remove">X</span></td>';
-    addElement += '</td>';
-
-    table.insertAdjacentHTML('beforeend', addElement);
+function createContactHTML(name, surname, phone) {
+    return TASK_ITEMS_TEMPLATE
+        .replace('{{name}}', name)
+        .replace('{{surname}}', surname)
+        .replace('{{phone}}', phone)
 }
 
-function clearForm() {
-    form.querySelectorAll(INPUT_CLASS).forEach(element => {
-        element.value = '';
+function getValue(input) {
+    return input.value;
+}
+
+function clearInputs(inputList) {
+    inputList.forEach(input => {
+        input.value = '';
     })
 }
 
+function isEmpty(inputList) {
+
+    for (let i = 0; i < inputList.length; i++) {
+        if (!inputList[i].value) {
+            errorEl.classList.remove('input-subtext');
+            errorEl.classList.add('error');
+            return true;
+        }
+    };
+
+    return false;
+}
+
+function clearError() {
+    errorEl.classList.remove('error');
+    errorEl.classList.add('input-subtext');
+}
+
 function removeRow(e) {
-    if (e.target.classList.contains(BTN_REMOVE_CLASS))
+    if (e.target.classList.contains('btn-remove'))
         e.target.closest('tr').remove();
 }
