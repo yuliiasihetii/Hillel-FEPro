d7652d92d8b46991e055a9c82f03877b7a03c8ea
yuliiasihetii <jsigetiy@gmail.com>
2022-05-14 09:20:00 +0300

    hm13

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
-let massage = prompt('What`s your name?');
+function Calculator(n) {
+    this.result = n;
+}
 
-alert('Hello,'+ massage + '! How are you?')
+Calculator.prototype.add = function (b) {
+    return this.result += b;
+}
+
+Calculator.prototype.mult = function (b) {
+    return this.result *= b;
+}
+
+Calculator.prototype.sub = function (b) {
+    return this.result -= b;
+}
+
+Calculator.prototype.div = function (b) {
+    return this.result /= b;
+}
+
+const calc = new Calculator(10);
